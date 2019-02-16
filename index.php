5d1895aedf5a55db01c25cf35c0b43918d7750ed
Alexander Velichko <alvi@mail.ua>
2019-02-16 23:43:25 +0200

    feat: added task2

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
+/**
+ * Generic_Sniffs_Methods_OpeningMethodBraceBsdAllmanSniff.
+ *
+ * PHP version 5
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Alexander Velichko <alvi@mail.ua>
+ * @copyright 2006 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @license   http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
+ * @version   CVS: $Id: OpeningFunctionBraceBsdAllmanSniff.php,v 1.8
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
+ */
 
 require_once 'src/functions.php';
 
-task1(['hi','i\'m','first','function'], true);
+task1(['hi', 'i\'m', 'first', 'function'], true);
+task2('+', 1.1, 2, 3, 5.9);
--- src/functions.php
+++ src/functions.php
@@ -1,7 +1,31 @@
 <?php
+/**
+ * Generic_Sniffs_Methods_OpeningMethodBraceBsdAllmanSniff.
+ *
+ * PHP version 5
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Alexander Velichko <alvi@mail.ua>
+ * @copyright 2006 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @license   http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
+ * @version   CVS: $Id: OpeningFunctionBraceBsdAllmanSniff.php,v 1.8
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
+ */
 
 
-function task1($array, $mark=false)
+CONST TEST_CONSTANTA = 'asd';
+$user_name = 'Igor';
+
+/**
+ * Description.
+ *
+ * @param array $array array of string
+ * @param bool  $mark  check for returning
+ *
+ * @return string
+ */
+function task1($array, $mark = false)
 {
     if (!empty($array)) {
         $string = '';
@@ -12,5 +36,40 @@ function task1($array, $mark=false)
             return $string;
         }
         echo $string;
+    }
+}
+
+/**
+ * Description.
+ *
+ * @return void
+ */
+function task2()
+{
+    $numbers = func_get_args();
+    $firstString = array_shift($numbers);
+    if ($firstString) {
+        switch ($firstString) {
+        case '+':
+            echo array_reduce(
+                $numbers, function ($carry, $item) {
+                    $carry += $item;
+                    return $carry;
+                }
+            );
+            break;
+        case '-':
+            echo array_reduce(
+                $numbers, function ($carry, $item) {
+                        $carry -= $item;
+                        return $carry;
+                }
+            );
+            break;
+        default:
+            echo 'Неверные параметры!';
+            break;
+        }
+
     }
 }
